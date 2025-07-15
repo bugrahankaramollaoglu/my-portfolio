cf4cc27418c9e08da032813a12fac5e4ac683ee2
buğrahan karamollaoğlu <bugrahankaramollaoglu@gmail.com>
2025-07-15 22:43:46 +0300

    changed bg color. will now try adding the new ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
+const geist = Geist({ subsets: ["latin"] });
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-white text-gray-800">
+    <html lang="en" className={geist.className}>
+      <body className="bg-gray-100 text-gray-800 min-h-screen">  {/* Light gray background */}
         <header className="p-4 shadow-md">
           <nav className="max-w-4xl mx-auto flex justify-between">
             <div className="font-bold text-xl">MyPortfolio</div>
