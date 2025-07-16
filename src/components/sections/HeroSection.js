96674c1887c2e4fb8ecb4c2a2e1dc433270ce7bb
buğrahan karamollaoğlu <bugrahankaramollaoglu@gmail.com>
2025-07-16 13:11:39 +0300

    hero section moved for better modularization. will move each

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { Inter, Lato, Lora, Oswald, Roboto, Ubuntu } from "next/font/google";
+import { Typewriter } from "@/components/ui/typewriter";
+import { SparklesCore } from "@/components/ui/sparkles"
+import { Mail, Github, Linkedin } from "lucide-react";
+import { FaMedium, FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+
+
+{/* Hero Section */ }
+
+export default function HeroSection({ oswald, inter }) {
+	return (
+
+		<section className="space-y-4 text-center">
+			<Image
+				src="/avatar.png"
+				alt="Buğrahan Karamollaoğlu"
+				width={200}
+				height={200}
+				className="mx-auto rounded-full border-4"
+			/>
+			<h1 className={`text-5xl font-bold ${oswald.className}`}>
+				Buğrahan Karamollaoğlu
+			</h1>
+			<h4 className={`text-3xl mb-15 text-blue-500 font-bold ${inter.className}`}>
+				<Typewriter delay={0.5} baseText="Fullstack Mobile Developer." />
+			</h4>
+			<h4 className={`text-3xl flex justify-center items-center gap-2 ${oswald.className}`}>
+				Native Android
+				<img src="/icons/android.png" alt="Android" className="h-6 w-6" />
+				•
+				Flutter
+				<img src="/icons/flutter.png" alt="Flutter" className="h-6 w-6" />
+				•
+				Django
+				<img src="/icons/django.png" alt="Django" className="h-6 w-6" />
+				•
+				Nodejs
+				<img src="/icons/nodejs.png" alt="Nodejs" className="h-6 w-6" />
+			</h4>
+		</section>
+
+	);
+}
